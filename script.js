342884dab3a3296c18b25293f5d056fedb5dd0e3
Toolverseai <ibrahimkhan52870@gmail.com>
2026-08-02 23:52:39 +0530

    Update script.jsAdd mobile menu toggle

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("ToolVerse AI Loaded 🚀");
+const menuBtn = document.getElementById("menuBtn");
+const navMenu = document.getElementById("navMenu");
+
+menuBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
