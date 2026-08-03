3039bdbf6e52f607f8047c79bddb1f00efcc8e86
Toolverseai <ibrahimkhan52870@gmail.com>
2026-08-03 21:45:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ const navMenu = document.getElementById("navMenu");
 menuBtn.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
+// Search AI Tools
+const searchInput = document.getElementById("searchInput");
+const toolCards = document.querySelectorAll(".tool-card");
+
+searchInput.addEventListener("input", () => {
+  const value = searchInput.value.toLowerCase();
+
+  toolCards.forEach((card) => {
+    const text = card.textContent.toLowerCase();
+
+    if (text.includes(value)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
